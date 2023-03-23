a4963d912aff17f60caaf89f9cfcbef49ed82e18
Wes <wfreemansmith@yahoo.co.uk>
2023-03-23 16:58:06 +0000

    components added

---
+++ life-cycle-degit/src/components/Poi.svelte
@@ -0,0 +1,26 @@
+<script>
+  export const poi = {}
+</script>
+
+<main>
+  <h1>{poi.eventName}</h1>
+  <p>{poi.detail}</p>
+</main>
+
+<style>
+  h1 {
+    color: #eff3f4;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+  main {
+    border-width: 2px;
+    border-style: solid;
+    border-color: #66686b;
+    width: fit-content;
+    padding: 20px 50px;
+    background-color: #f1ae56;
+    margin: 0px 50px;
+  }
+</style>
---
+++ life-cycle-degit/src/components/Tree.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Poi from "./Poi.svelte";
+  export let tree = [];
+</script>
+
+<main>
+  {#each tree as poi}
+    <Poi {poi} />
+  {/each}
+</main>
+
+<style>
+  main {
+    list-style: none;
+    padding: 0px;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    align-content: flex-start;
+    justify-content: flex-start;
+  }
+</style>
